608ad8b5eb1468f1522e22758cda1aa4e2c40e40
IsaacMasisV <isaacmv2003@gmail.com>
2024-05-02 13:33:23 -0600

    jueves

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ if (checked==true){
     label_toggle.style.color="yellow";
     }else{
         label_toggle.innerHTML='<i class="fa-solid fa-moon"></i>';
-        label_toggle.style.color="rebeccapurple";
+        label_toggle.style.color="white";
     }
 })
